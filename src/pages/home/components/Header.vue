5f97e3729b451c95cc4e0908e3b66423feef404d
ChillyBreeze1998 <1342327426@qq.com>
2019-04-10 22:44:17 +0800

    add header

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,15 @@
 <template>
-<div class="header">11
-   <div class="header-left"></div>
-   <div class="header-input"></div>
-   <div class="header-right"></div>
+<div class="header">
+   <div class="header-left">
+     <div class="iconfont back-icon">&#xe624;</div>
+   </div>
+   <div class="header-input">
+     <span class="iconfont">&#xe632;</span>
+     城市/景点/游玩主题
+     </div>
+   <div class="header-right">城市
+     <span class="iconfont xiaJianTou-icon">&#xeb6d;</span>
+   </div>
 </div>
 </template>
 
@@ -12,8 +19,39 @@ export default {
 }
 </script>
 
-<style scoped lang="stylus">
- 1rem = html font-size = 50px
-.header
-  height: .86rem
+<style lang="stylus" scoped>
+@import '~styles/QuanJu.styl';
+//头所有样式
+  .header
+    display: flex
+    // 原来是86px，设计师给的图是2倍的，所以除以2,1rem =50px，所以43px除50就是rem的大小
+    line-height  .86rem
+    background  $bgColor
+    color #fff
+//头左边样式
+  .header-left
+     width  .64rem
+     float  left
+  .back-icon
+     text-align center
+     font-size .4rem
+     padding-left 0.2rem
+//搜索栏样式
+  .header-input
+     line-height .64rem
+     flex 1
+     background-color #fff
+     border-radius 0.1rem
+     color #ccc
+     margin-top .12rem
+     margin-bottom .12rem
+     text-align center
+     margin-left .2rem
+//头右边样式
+  .header-right
+    width  1.24rem
+    float right
+    text-align center
+  .xiaJianTou-icon
+    font-size .3rem
  </style>
