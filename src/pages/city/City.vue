daa8ef11af19dadda701bb6b7a5e31cdb51cc1d4
ChillyBreeze1998 <1342327426@qq.com>
2019-04-13 16:45:58 +0800

    实现城市页面和列表通过ajax请求获得

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
     <div>
       <city-header></city-header>
       <city-search></city-search>
-      <city-list></city-list>
-      <city-alphabet></city-alphabet>
+      <city-list :cities="cities" :hot="hotCities"></city-list>
+      <city-alphabet  :cities="cities"></city-alphabet> <!-- 调用字母表 -->
     </div>
 </template>
 
@@ -13,6 +13,7 @@ import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
 import CityAlphabet from './components/Alphabet'
+import axios from 'axios'
 export default {
   name: 'City',
   components: {
@@ -20,6 +21,32 @@ export default {
     CitySearch,
     CityList,
     CityAlphabet
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: []
+    }
+  },
+  methods: {
+    // 请求
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then(this.handleGetCityInfoSucc)
+    },
+    // 获取
+    handleGetCityInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    }
+  },
+  mounted () {
+    // 调用 在生命周期函数安装这个函数里写一个方法的调用
+    this.getCityInfo()
   }
 }
 </script>
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -9,48 +9,21 @@
              </div>
          </div>
       </div>
+       <!-- ajax获取热门城市的信息 -->
       <div class="area">
           <div class="title" border-topbuttom>热门城市</div>
            <div class="button-list">
-                  <div class="button-wrapper">
-                  <div class="button">北京</div>
-             </div>
-             <div class="button-wrapper">
-                  <div class="button">北京</div>
-             </div>
-             <div class="button-wrapper">
-                  <div class="button">北京</div>
-             </div>
-             <div class="button-wrapper">
-                  <div class="button">北京</div>
-             </div>
-             <div class="button-wrapper">
-                  <div class="button">北京</div>
+                  <div class="button-wrapper" v-for="item in hot" :key="item.id">
+                  <div class="button">{{item.name}}</div>
              </div>
             </div>
           </div>
-     <div class="area">
-          <div class="title" border-topbuttom>A</div>
+          <!-- ajax获取字母对应区域城市的信息 -->
+     <div class="area" v-for="(item, key) in cities" :key="key">
+          <div class="title" border-topbuttom>{{key}}</div>
           <div class="item-list">
-              <div class="item border-bottom">阿拉尔</div>
-              <div class="item border-bottom">阿拉尔</div>
-              <div class="item border-bottom">阿拉尔</div>
-              <div class="item border-bottom">阿拉尔</div>
-              <div class="item border-bottom">阿拉尔</div>
-              <div class="item border-bottom">阿拉尔</div>
-              <div class="item border-bottom">阿拉尔</div>
-          </div>
-      </div>
-       <div class="area">
-          <div class="title" border-topbuttom>B</div>
-          <div class="item-list">
-              <div class="item border-bottom">阿拉尔</div>
-              <div class="item border-bottom">阿拉尔</div>
-              <div class="item border-bottom">阿拉尔</div>
-              <div class="item border-bottom">阿拉尔</div>
-              <div class="item border-bottom">阿拉尔</div>
-              <div class="item border-bottom">阿拉尔</div>
-              <div class="item border-bottom">阿拉尔</div>
+              <!-- 二次循环继续循环key(城市字母)内部的值 -->
+              <div class="item border-bottom" v-for="innerItem in item" :key="innerItem.id">{{innerItem.name}}</div>
           </div>
       </div>
   </div>
@@ -61,6 +34,10 @@
 import Bscroll from 'better-scroll'
 export default {
   name: 'CityList',
+  props: {
+    cities: Object,
+    hot: Array
+  },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
   }
